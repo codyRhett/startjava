c85c7255af049e9e841b10a165f169a769a2a964
codyRhett <artemryvkin@yandex.ru>
2021-01-26 20:10:54 +0300

    Перенес геттеры и сеттеры. Сделал правильные отступы

--- lesson_2/Wolf_2.java
+++ lesson_2/Wolf_2.java
@@ -12,6 +12,50 @@ public class Wolf_2 {
     private int age;
     private String color;
 
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setNickname(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getNickname() {
+        return nickName;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+        System.out.println("Age is not correct");
+        } else {
+        this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
     public void walk() {
 
     }
@@ -30,49 +74,5 @@ public class Wolf_2 {
 
     public void hunt() {
 
-    }
-
-    public void setGender(char gender) {
-      this.gender = gender;
-    }
-
-    public char getGender() {
-      return gender;
-    }
-
-    public void setNickname(String nickName) {
-      this.nickName = nickName;
-    }
-
-    public String getNickname() {
-      return nickName;
-    }
-
-    public void setWeight(float weight) {
-      this.weight = weight;
-    }
-
-    public float getWeight() {
-      return weight;
-    }
-
-    public void setAge(int age) {
-      if (age > 8) {
-        System.out.println("Age is not correct");
-      } else {
-        this.age = age;
-      }
-    }
-
-    public int getAge() {
-      return age;
-    }
-
-    public void setColor(String color) {
-      this.color = color;
-    }
-    
-    public String getColor() {
-      return color;
     }
 }
