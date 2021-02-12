abced2b4e1f55935bcaa572ebaf61615c3502b88
codyRhett <artemryvkin@yandex.ru>
2021-02-12 18:14:51 +0300

    вернул диапазон на 100

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -21,8 +21,7 @@ public class GuessNumber {
 
     public void start() {
         Scanner scan = new Scanner(System.in);
-        //secretNumber = new Random().nextInt(99 + 1) + 1;
-        secretNumber = new Random().nextInt(4 + 1) + 1;
+        secretNumber = new Random().nextInt(100) + 1;
         while(true) {
             System.out.print("Игрок " + player1.getName() + ", введите число: ");
             player1.setNumber(scan.nextInt());
